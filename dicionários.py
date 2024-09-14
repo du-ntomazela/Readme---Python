348b9514836ba321d9c10992a648295db62af2cc
tomazelaeduardon@gmail.com <tomazelaeduardon@gmail.com>
2024-09-14 16:15:47 -0300

    Commit no codigo fonte - 1

---
+++ dicionários.py
@@ -0,0 +1,27 @@
+noticias = {
+    "Notícia1": {
+                 "Título":
+                     "F-E: Cofundador promete evento 'maior e melhor' em São Paulo",
+                 "Conteúdo":
+                     "São Paulo será a grande estreia da temporada 2024-25 da Fórmula E.  Alberto Longo, cofundador da categoria de monopostos elétricos, afirmou, em coletiva de imprensa, que evento será 'maior e melhor'. O EP inaugural acontecerá no dia 7 de dezembro deste ano, no sambódromo do Anhembi. Longo elogiou o EP deste ano, que aconteceu em março, citando como a capital paulista acolhe a categoria e é apaixonada pelo automobilismo 'Vocês nos fazem sentir em casa.' 'Não sou só eu que falo isso, como cofundador da F-E, mas falo em nome das escuderias, dos pilotos e das pessoas que assistem e seguem essa paixão'"
+                 },
+    "Notícia2": {
+                 "Título":
+                     "Fórmula E: Guenther é anunciado na Penske como companheiro de Vergne",
+                 "Conteúdo":
+                     "O alemão substituirá o campeão de 2021/22 Stoffel Vandoorne, que foi confirmado na Maserati MSG em julho. O piloto de 27 anos chega a equipe ítalo-monegasca depois de passar duas temporadas na Maserati, onde venceu duas corridas - Jacarta e Tóquio - mas apresentou incosistências para gaugar uma disputa pelo título, terminando em sétimo e oitavo na classificação de pilotos, respectivamente. "
+                 },
+    "Notícia3": {
+                 "Título":
+                     "F1 - Pourchaire sobre falta de oportunidade: 'Só estou pedindo um assento'",
+                 "Conteúdo":
+                     "Théo Pourchaire, piloto francês campeão da Fórmula 2 em 2023, contou, em entrevista exclusiva ao Motorsport.com, sobre o ano de 2024, correndo pela Super Fórmula japonesa, e aguardando pela oportunidade na Fórmula 1. O piloto júnior da Sauber demontrou insatisfação que outros pilotos jovens, como Andrea Kimi Antonelli e Oliver Bearman, terem oportunidades antes dele, pulando etapas na F2. Confira conversa completa com Pourchaire."
+                 },
+    "Notícia4": {
+                 "Título":
+                     "Vice-campeão da F-E comenta permanência de Pérez na Red Bull: 'Continua trazendo dinheiro'",
+                 "Conteúdo":
+                     "Em conversa com o Total Motorsport, o vice-campeão da F-E disse que novos talentos não tem chance de chegar à Fórmula 1 porque não contam com o apoio de grandes patrocinadores e não desejam fazer o trabalho de piloto reserva. Evans decidiu trocar a F1 pela F-E aos 22 anos e continuará por lá, visto que as portas da mais alta categoria de automobilismo nunca chegaram a se abrir de fato para ele. Mitch aponta que a questão financeira é uma das causas para isso."
+                 },
+
+}
--- sprint.py
+++ sprint.py
@@ -1,4 +1,5 @@
 from matrizes import logo
+from dicionários import noticias
 
 def num_int(num, msg):
     while True:
@@ -6,3 +7,22 @@ def num_int(num, msg):
         if x.isnumeric():
             break
 
+def login_user ():
+    user_list = []
+    password_list = []
+    while True:
+        teste = input("Você ja possui uma conta? (responda com sim ou não) \n ->")
+        if teste in ["sim", "não"]:
+            break
+    if teste == "sim":
+        user = input("Informe o seu usuário: ")
+        password = input("Informe a sua senha: ")
+        if user in user_list and password in password_list:
+            return user
+        else:
+            print("Usuario ou senha não correspondentes!")
+    else:
+        new_user = input("Informe um usuário: ")
+        new_password = input("Informe uma senha: ")
+        confirmn_password = input("Confirme a sua senha")
+        
